a2bbce61d38612348c67aea8bc622827324310c3
raedsalah <dkraed@gmail.com>
2024-11-14 20:26:08 +0800

    [feat] button replaced with icon

--- web/src/components/SearchHistory.tsx
+++ web/src/components/SearchHistory.tsx
@@ -4,12 +4,13 @@ import {
   List,
   ListItem,
   ListItemText,
-  Button,
   Snackbar,
   Alert,
+  IconButton,
 } from "@mui/material";
 import { AppDispatch, RootState } from "../store";
 import { addFavorite, Place, selectPlace } from "../store/slice/searchesSlice";
+import { StarBorder, Star } from "@mui/icons-material";
 
 const SearchHistory: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -65,22 +66,29 @@ const SearchHistory: React.FC = () => {
     <>
       <List>
         {history.map((place, index) => {
-          console.log(place);
+          const isFavorited = place.place_id
+            ? favorites.includes(place.place_id)
+            : false;
           return (
             <ListItem
               key={place.place_id || index}
               onClick={() => handleSelectPlace(place)}
               secondaryAction={
                 place.place_id ? (
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => handleFavorite(place.place_id!)}
+                  <IconButton
+                    edge="end"
+                    aria-label="favorite"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleFavorite(place.place_id!);
+                    }}
                   >
-                    {favorites.includes(place.place_id!)
-                      ? "Favorited"
-                      : "Favorite"}
-                  </Button>
+                    {isFavorited ? (
+                      <Star sx={{ color: "gold" }} />
+                    ) : (
+                      <StarBorder />
+                    )}
+                  </IconButton>
                 ) : null
               }
             >
