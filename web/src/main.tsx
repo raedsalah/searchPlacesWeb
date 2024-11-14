9ff48e507b8e832f3bc41ccaef274679c7730d87
raedsalah <dkraed@gmail.com>
2024-11-14 17:35:48 +0800

    [fix] missing mui base styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
+import CssBaseline from "@mui/material/CssBaseline";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <CssBaseline />
       <App />
     </Provider>
   </StrictMode>
